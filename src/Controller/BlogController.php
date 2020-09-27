891c07061995ff3b7f24906d91bf5bbd33256376
mistrnex <38112062+mistrnex@users.noreply.github.com>
2020-09-28 00:39:18 +0200

    - changes in view templates but really working

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,17 +8,19 @@ use App\Service\Greeting;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Class BlogController
  * @Route ("/blog")
  */
-class BlogController
+class BlogController extends AbstractController
 {
     /**
      * @var \Twig\Environment
@@ -30,7 +32,21 @@ class BlogController
      */
     private $session;
 
-    public function __construct(\Twig\Environment $twig, SessionInterface $session)
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * BlogController constructor.
+     * @param \Twig\Environment $twig
+     * @param SessionInterface $session
+     * @param RouterInterface $router
+     */
+    public function __construct(
+        \Twig\Environment $twig,
+        SessionInterface $session,
+        RouterInterface $router)
     {
         $this->twig = $twig;
         $this->session = $session;
@@ -38,10 +54,10 @@ class BlogController
 
     /**
      * First method for blog controller testing
-     *
-     * @Route("/{name}", name="blog_index")
+     * @Method({"GET"})
+     * @Route("/", name="blog_index")
      */
-    public function index($name)
+    public function index()
     {
         $html = $this->twig->render('blog/index.html.twig',
         [
@@ -49,7 +65,11 @@ class BlogController
         ]
         );
 
-        return new Response($html);
+        $posts = $this->session->get('posts');
+
+//        return new Response($html);
+
+        return $this->render('articles/index.html.twig', array('posts' => $posts));
     }
 
     /**
@@ -59,11 +79,13 @@ class BlogController
     public function add()
     {
         $posts = $this->session->get('posts');
-        $posts[uniqid()] = [
+        $posts[uniqid('', true)] = [
             'title' => 'A random title ' . random_int(1, 500),
             'text' => 'Some random text nr. ' . random_int(1, 500),
         ];
         $this->session->set('posts', $posts);
+
+        return new RedirectResponse($this->router->generate('blog_index'));
     }
 
     /**
